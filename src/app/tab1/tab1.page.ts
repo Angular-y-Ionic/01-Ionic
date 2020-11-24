e9b3ba8d43bfdd6c8cd8b0f6df3c9bf790578af6
Eduardo Gonzalez <eduardsalvadorgonzalez@gmail.com>
2020-11-24 11:50:54 -0600

    prueba

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
- 
+ // Hola prueba
   constructor() {} 
 
   ngOnInit(): void {
